46b09e2c77b930266f469d21114d72e50668f041
Cass <bikbaevf18@gmail.com>
2023-11-01 21:28:15 +0400

    Add HomeLinks

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -2,12 +2,14 @@ import { PhoneTemplate } from '@/src/features/phone-template';
 import { UIPageLayout } from '@/src/shared/ui/layouts/ui-page-layout';
 import { Header } from '@/src/widgets/header';
 
+import { HomeLinks } from './ui';
+
 export const Home = () => {
   return (
     <UIPageLayout
       header={<Header />}
       left={<PhoneTemplate />}
-      right={<p>Test</p>}
+      right={<HomeLinks />}
     />
   );
 };
---
+++ src/pages/home/ui/home-links.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+
+import styles from './home-links.module.scss';
+
+interface IProps {}
+
+export const HomeLinks: FC<IProps> = () => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Customize your links</h1>
+        <p className={styles.subtitle}>
+          Add/edit/remove links below and then share all your profiles with the
+          world!
+        </p>
+      </header>
+    </div>
+  );
+};
