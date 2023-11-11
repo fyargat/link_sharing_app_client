1afd4d5aada766f688ad9fd754f2f892242d2543
Cass <bikbaevf18@gmail.com>
2023-11-11 13:23:58 +0400

    Update ui-sign-out-button

--- src/features/sign-out/ui/ui-sign-out-button/ui-sign-out-button.tsx
+++ src/features/sign-out/ui/ui-sign-out-button/ui-sign-out-button.tsx
@@ -1,5 +1,11 @@
 import { FC } from 'react';
 
+import { ICONS } from '@/src/shared/config/icons';
+import { UIButtonSpinner } from '@/src/shared/ui/ui-button-spinner';
+import { UIPrimaryButton } from '@/src/shared/ui/ui-primary-button';
+
+import styles from './ui-sign-out-button.module.scss';
+
 interface IProps {
   onSignOut: () => void;
   isLoading: boolean;
@@ -7,8 +13,18 @@ interface IProps {
 
 export const UISignOutButton: FC<IProps> = ({ onSignOut, isLoading }) => {
   return (
-    <button onClick={onSignOut} disabled={isLoading}>
-      Sign Out
-    </button>
+    <UIPrimaryButton
+      className={styles.button}
+      onClick={onSignOut}
+      disabled={isLoading}
+    >
+      {isLoading && (
+        <div className={styles.spinner}>
+          <UIButtonSpinner />
+        </div>
+      )}
+      <div className={styles.icon}>{ICONS['logout']}</div>
+      <p className={styles.text}>Sign Out</p>
+    </UIPrimaryButton>
   );
 };
