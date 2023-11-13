85010b78e56322be3924958020f5de56e062bf03
Cass <bikbaevf18@gmail.com>
2023-11-13 21:27:45 +0400

    Add edge storage

--- src/app/provider/app-provider.tsx
+++ src/app/provider/app-provider.tsx
@@ -5,12 +5,17 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastContainer } from 'react-toastify';
 
 import { queryClient } from '@/src/shared/api/query-client';
+import { EdgeStoreProvider } from '@/src/shared/lib/edgestore';
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
-      <ToastContainer />
+      <EdgeStoreProvider>{children}</EdgeStoreProvider>
+      <ToastContainer
+        autoClose={1000}
+        hideProgressBar={true}
+        pauseOnHover={false}
+      />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
