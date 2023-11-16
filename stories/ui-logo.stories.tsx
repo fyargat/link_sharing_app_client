29aa781303d577eaa083655141bf8d143fb43f1d
Cass <bikbaevf18@gmail.com>
2023-11-16 17:19:17 +0400

    Remove ui-logo & ui-navbar-link stories

--- stories/ui-logo.stories.tsx
+++ stories/ui-logo.stories.tsx
@@ -1,5 +1,6 @@
-import { UILogo } from '@/src/shared/ui/ui-logo';
 import type { Meta, StoryObj } from '@storybook/react';
+
+import { UILogo } from '@/src/shared/ui/ui-logo';
 
 const meta = {
   title: 'UI/Logo',
@@ -13,7 +14,15 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  decorators: [
+    (Story) => (
+      <div style={{ width: 40 }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
 
 export const Full: Story = {
   args: {
