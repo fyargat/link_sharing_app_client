7755b32cc1c0c9d8304e02f1222e760d84ae497e
Cass <bikbaevf18@gmail.com>
2023-11-19 22:39:06 +0400

    Update footer

--- src/shared/ui/ui-footer/ui-footer.tsx
+++ src/shared/ui/ui-footer/ui-footer.tsx
@@ -1,9 +1,30 @@
 import { FC } from 'react';
 
+import { getCurrentYear } from '../../lib/time';
+import { UILogo } from '../ui-logo';
 import styles from './ui-footer.module.scss';
 
 interface IProps {}
 
 export const UIFooter: FC<IProps> = () => {
-  return <footer className={styles.footer}>Footer</footer>;
+  const currentYear = getCurrentYear();
+
+  return (
+    <footer className={styles.footer}>
+      <UILogo />
+
+      <p>
+        {' '}
+        {currentYear} · Developed by{' '}
+        <a
+          className={styles.link}
+          href='https://fyargat.com/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Fyargat Bikbaev
+        </a>{' '}
+      </p>
+    </footer>
+  );
 };
